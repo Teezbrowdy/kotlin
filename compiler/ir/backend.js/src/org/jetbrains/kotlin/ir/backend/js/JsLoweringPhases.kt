5a45ffb4b22b6a837845f3913830eb1c27ec2259
Artem Kobzar <artem.kobzar@jetbrains.com>
2023-05-17 18:14:50 +0200

    [K/JS] Support KClass<*>.createInstance reflection method

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/JsLoweringPhases.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/JsLoweringPhases.kt
@@ -993,6 +993,18 @@ private val es6PrimaryConstructorOptimizationLowering = makeDeclarationTransform
     prerequisite = setOf(es6CollectPrimaryConstructorsWhichCouldBeOptimizedLowering)
 )
 
+private val collectionClassesWhichRequiresExtendedMetadataLowering = makeBodyLoweringPhase(
+    ::CollectClassesWhichRequiresExtendedMetadataLowering,
+    name = "CollectClassesWhichRequiresExtendedMetadataLowering",
+    description = "[Optimization] Collect classes which use ::class",
+)
+
+private val skipExtendedMetadataLowering = makeDeclarationTransformerPhase(
+    ::SkipExtendedMetadataLowering,
+    name = "SkipExtendedMetadataLowering",
+    description = "[Optimization] Mark classes which could live without defaultConstructor in metadata",
+)
+
 private val es6PrimaryConstructorUsageOptimizationLowering = makeBodyLoweringPhase(
     ::ES6PrimaryConstructorUsageOptimizationLowering,
     name = "ES6PrimaryConstructorUsageOptimizationLowering",
@@ -1020,7 +1032,9 @@ val optimizationLoweringList = listOf<Lowering>(
     es6PrimaryConstructorOptimizationLowering,
     es6PrimaryConstructorUsageOptimizationLowering,
     purifyObjectInstanceGetters,
-    inlineObjectsWithPureInitialization
+    inlineObjectsWithPureInitialization,
+    collectionClassesWhichRequiresExtendedMetadataLowering,
+    skipExtendedMetadataLowering
 )
 
 val jsOptimizationPhases = SameTypeNamedCompilerPhase(
