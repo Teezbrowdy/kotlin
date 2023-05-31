3b00c600541ffdd973e1348ce0aabc8f439e6fe4
Denis.Zharkov <denis.zharkov@jetbrains.com>
2023-05-31 18:28:29 +0200

    K2: Fix isHiddenForThisCallSite for non-direct overridden
    Previously, the call of `z.sort` in test has been resolved
    to the overridden member

--- compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirLightTreeOldFrontendDiagnosticsTestGenerated.java
+++ compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirLightTreeOldFrontendDiagnosticsTestGenerated.java
@@ -20320,6 +20320,12 @@ public class FirLightTreeOldFrontendDiagnosticsTestGenerated extends AbstractFir
                 }
 
                 @Test
+                @TestMetadata("nonDirectHiddenOverride.kt")
+                public void testNonDirectHiddenOverride() throws Exception {
+                    runTest("compiler/testData/diagnostics/tests/j+k/collectionOverrides/nonDirectHiddenOverride.kt");
+                }
+
+                @Test
                 @TestMetadata("overridesBuiltinNoMagic.kt")
                 public void testOverridesBuiltinNoMagic() throws Exception {
                     runTest("compiler/testData/diagnostics/tests/j+k/collectionOverrides/overridesBuiltinNoMagic.kt");
