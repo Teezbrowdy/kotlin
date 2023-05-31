5940770e9ad8d4c125faf3f69579c0c2bcfd07ef
Troels Bjerre Lund <troels@google.com>
2023-05-31 13:00:05 +0200

    Fix getting Argument info from field annotation

--- build-common/src/org/jetbrains/kotlin/idea/explicitDefaultSubstitutors.kt
+++ build-common/src/org/jetbrains/kotlin/idea/explicitDefaultSubstitutors.kt
@@ -12,6 +12,7 @@ import org.jetbrains.kotlin.config.JvmTarget
 import kotlin.reflect.KClass
 import kotlin.reflect.KProperty1
 import kotlin.reflect.full.findAnnotation
+import kotlin.reflect.jvm.javaField
 
 val defaultSubstitutors: Map<KClass<out CommonToolArguments>, Collection<ExplicitDefaultSubstitutor>> =
     mapOf(K2JVMCompilerArguments::class to listOf(JvmTargetDefaultSubstitutor))
@@ -23,7 +24,8 @@ sealed class ExplicitDefaultSubstitutor {
     abstract fun isSubstitutable(args: List<String>): Boolean
 
     protected val argument: Argument by lazy {
-        substitutedProperty.findAnnotation() ?: error("Property \"${substitutedProperty.name}\" has no Argument annotation")
+        substitutedProperty.javaField?.getAnnotation(Argument::class.java)
+            ?: error("Property \"${substitutedProperty.name}\" has no Argument annotation")
     }
 }
 
